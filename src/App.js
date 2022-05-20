1850870d84e75af78f90206ab66027890c596431
AbdussamadYisau <sammieyisau@gmail.com>
2022-05-21 00:21:00 +0100

    Added Modal for form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@
 import React from "react";
 import "./App.css";
 import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
 
 function App() {
   const [mainInventoryData, setMainInventoryData] = React.useState(null);
@@ -11,9 +18,42 @@ function App() {
   const [deleted, setDeleted] = React.useState("");
   const [restore, setRestore] = React.useState("");
   const [loading, setLoading] = React.useState(false);
+  const [modalOpen, setIsOpen] = React.useState(false);
+  const [formData, setFormData] = React.useReducer(formReducer, {});
 
-  
-  
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    
+    setIsOpen(false);
+  };
+
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  };
+
+  const Modal = ({ handleClose, children, title }) => {
+    return (
+      <div className={`modalOpen`}>
+        <div className="modalContent">
+          <span className="modalClose" onClick={handleClose}>
+            &times;
+          </span>
+          <h3 className="modalHeader">{title}</h3>
+          {children}
+        </div>
+      </div>
+    );
+  };
 
   const getInventory = async () => {
     try {
@@ -30,10 +70,7 @@ function App() {
 
   const getDeletedInventory = async () => {
     try {
-      const response = await fetch(
-        "v1/api/deletedInventory",
-        { mode: "cors" }
-      );
+      const response = await fetch("v1/api/deletedInventory", { mode: "cors" });
       const json = await response.json();
       setDeleteInventoryData(json?.data);
     } catch (err) {
@@ -49,6 +86,38 @@ function App() {
     getInventory();
     getDeletedInventory();
   }, [deleted, restore, loading]);
+
+
+
+  const handleCreate = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `v1/api/inventory`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: `${formData?.name}`,
+            description: `${formData?.description}`,
+            price: `${formData?.price}`,
+            quantity: `${formData?.quantity}`
+
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+        {
+          mode: "cors",
+        }
+      );
+      const data = await response.json();
+
+      setLoading(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 
   const handleDelete = async (id) => {
     try {
@@ -77,7 +146,7 @@ function App() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        // transition: "slide",
+        
       });
 
       setDeleted(data.message);
@@ -109,7 +178,6 @@ function App() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        // transition: "slide",
       });
       setRestore(data.message);
       setLoading(false);
@@ -119,104 +187,137 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <ToastContainer />;
-      <div className="Column">
-        <h1>Main Inventory</h1>
-        <div>
-          {!mainInventoryData ? (
-            "Loading..."
-          ) : (
-            <>
-              {mainInventoryData.length === 0 ? (
-                <p>No Inventory items</p>
-              ) : (
-                <>
-                  {mainInventoryData?.map((item, index) => (
-                    <div
-                      key={item._id}
-                      style={{
-                        border: "2px solid red",
-                        borderRadius: "25px",
-                        padding: "10px",
-                        marginTop: "10px",
-                        display: "flex",
-                        width: "500px",
-                      }}
-                    >
-                      <div className="Column">
-                        <p> Name: {item.name}</p>
-                        <p> Description : {item.description}</p>
-                        <p> Price : {item.price} &#8358;</p>
-                        <p> Quantity: {item.quantity}</p>
-                      </div>
+    <>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "darkmagenta",
+          padding: "20px",
+          color: "white",
+          border: "2px solid darkmagenta",
+          borderRadius: "25px",
+          width: "250px",
+          height: "70px",
+          boxShadow: "none",
+          display: "block",
+          margin: "0 auto",
+        }}
+        onClick={showModal}
+      >
+        {" "}
+        Click to create an inventory{" "}
+      </button>
+      {modalOpen && (
+        <Modal handleClose={hideModal} title="Testing Microphone">
+          <p>Modal</p>
+          <p>Data</p>
+        </Modal>
+      )}
 
-                      <div className="Column">
-                        <button
-                          onClick={() => console.log("Hi")}
-                          style={{ marginRight: "10px" }}
-                        >
-                          Edit
-                        </button>
-                        <button onClick={() => handleDelete(item._id)}>
-                          Delete
-                        </button>
+      <div className="App">
+        <ToastContainer />;
+        <div className="Column">
+          <h1>Main Inventory</h1>
+          <div>
+            {!mainInventoryData ? (
+              "Loading..."
+            ) : (
+              <>
+                {mainInventoryData.length === 0 ? (
+                  <p>No Inventory items</p>
+                ) : (
+                  <>
+                    {mainInventoryData?.map((item, index) => (
+                      <div
+                        key={item._id}
+                        style={{
+                          border: "2px solid red",
+                          borderRadius: "25px",
+                          padding: "10px",
+                          marginTop: "10px",
+                          display: "flex",
+                          width: "500px",
+                        }}
+                      >
+                        <div className="Column">
+                          <p> Name: {item.name}</p>
+                          <p> Description : {item.description}</p>
+                          <p>
+                            {" "}
+                            Price : &#8358; {numberWithCommas(item.price)}{" "}
+                          </p>
+                          <p> Quantity: {item.quantity}</p>
+                        </div>
+
+                        <div className="Column">
+                          <button
+                            onClick={() => console.log("Hi")}
+                            style={{ marginRight: "10px" }}
+                          >
+                            Edit
+                          </button>
+                          <button onClick={() => handleDelete(item._id)}>
+                            Delete
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </>
-              )}
-            </>
-          )}
+                    ))}
+                  </>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+        <div className="Column">
+          <h2>Deleted Inventory</h2>
+
+          <div>
+            {!deleteInventoryData ? (
+              "Loading..."
+            ) : (
+              <>
+                {deleteInventoryData.length === 0 ? (
+                  <p>No deleted items</p>
+                ) : (
+                  <>
+                    {deleteInventoryData?.map((item, index) => (
+                      <div
+                        key={item._id}
+                        style={{
+                          border: "2px solid red",
+                          borderRadius: "25px",
+                          padding: "10px",
+                          marginTop: "10px",
+                          display: "flex",
+                          width: "500px",
+                        }}
+                      >
+                        <div className="Column">
+                          <p> Name: {item.name}</p>
+                          <p> Description : {item.description}</p>
+                          <p>
+                            {" "}
+                            Price : &#8358; {numberWithCommas(item.price)}{" "}
+                          </p>
+                          <p> Quantity: {item.quantity}</p>
+                          <p>Delete Comments: {item.deleteComment}</p>
+                        </div>
+
+                        <div>
+                          <button onClick={() => handleRestore(item._id)}>
+                            Undo
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                )}
+              </>
+            )}
+          </div>
         </div>
       </div>
-
-      <div className="Column">
-        <h2>Deleted Inventory</h2>
-
-        <div>
-          {!deleteInventoryData ? (
-            "Loading..."
-          ) : (
-            <>
-              {deleteInventoryData.length === 0 ? (
-                <p>No deleted items</p>
-              ) : (
-                <>
-                  {deleteInventoryData?.map((item, index) => (
-                    <div
-                      key={item._id}
-                      style={{
-                        border: "2px solid red",
-                        borderRadius: "25px",
-                        padding: "10px",
-                        marginTop: "10px",
-                        display: "flex",
-                        width: "500px",
-                      }}
-                    >
-                      <div className="Column">
-                        <p> Name: {item.name}</p>
-                        <p> Description : {item.description}</p>
-                        <p> Price : {item.price} &#8358;</p>
-                        <p> Quantity: {item.quantity}</p>
-                        <p>Delete Comments: {item.deleteComment}</p>
-                      </div>
-
-                      <div>
-                        <button onClick={() => handleRestore(item._id)}>
-                          Undo
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-                </>
-              )}
-            </>
-          )}
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
